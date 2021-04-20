31489b4ef7be3748f071fc7a8fba9b34fcd24baf
Mateus Fugita <mateushiwao@gmail.com>
2021-04-20 20:01:20 -0300

    create UserIdentification and Confirmation pages

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
 
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps {
     title: string;
 }
 
 export function Button({ title, ...rest }: ButtonProps){
-    return(
+    return (
         <TouchableOpacity 
-            style={styles.button}
-            activeOpacity={0.7}
+            style={styles.container}
             {...rest}
         >
-            <Text style={styles.buttonText}>
+            <Text style={styles.text}>
                 { title }
             </Text>
         </TouchableOpacity>
@@ -22,17 +22,16 @@ export function Button({ title, ...rest }: ButtonProps){
 }
 
 const styles = StyleSheet.create({
-    button: {
+    container: {
         backgroundColor: colors.green,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
         height: 56,
-        width: 56
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    buttonText: {
+    text: {
+        fontSize: 16,
         color: colors.white,
-        fontSize: 24
+        fontFamily: fonts.heading
     }
 })
