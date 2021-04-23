09a4d61a2a558bd324409266a51b4c5b0a96d588
Mateus Fugita <mateushiwao@gmail.com>
2021-04-23 16:15:26 -0300

    send notification after scheduling

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
 
@@ -17,6 +18,7 @@ const AuthRoutes = () => {
                 inactiveTintColor: colors.heading,
                 labelPosition: 'beside-icon',
                 style: {
+                    paddingVertical: Platform.OS === 'ios' ? 20 : 0,
                     height: 88,
                 }
             }}   
